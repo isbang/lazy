de1900b5eacc443c28452c60a09494b300e848f3
isbang <ethan@daangn.com>
2021-07-17 23:33:59 +0900

    feat(job): add default members to empty job

--- job.go
+++ job.go
@@ -1,11 +1,20 @@
 package lazy
 
+import "time"
+
 type JobInterface interface {
 	isLazyJob()
 	LazyQueueName() string
 }
 
-type EmptyJob struct{}
+type EmptyJob struct {
+	// Attempts is the number of times the job was executed by the lazy server.
+	// Before execute handlers, Attempts grows up.
+	Attempts int `json:"attempts"`
+
+	// CreatedAT is the time the job was created.
+	CreatedAT time.Time `json:"created_at"`
+}
 
 func (EmptyJob) isLazyJob() {}
 
