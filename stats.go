721c349d126441416da58519c43a8e68c8046e25
isbang <ethan@daangn.com>
2021-08-16 13:35:39 +0900

    feat: add stats type

---
+++ stats.go
@@ -0,0 +1,44 @@
+package lazy
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type Stats struct {
+	Running int64
+	Delayed int64
+	Dead    int64
+}
+
+func getQueueStats(ctx context.Context, cc redis.UniversalClient, name string) (*Stats, error) {
+	pipe := cc.Pipeline()
+	runningCmd := pipe.LLen(ctx, queuePrefix+name)
+	delayedCmd := pipe.ZCard(ctx, delayPrefix+name)
+	deadCmd := pipe.ZCard(ctx, deadPrefix+name)
+	if _, err := pipe.Exec(ctx); err != nil {
+		return nil, err
+	}
+
+	var stats Stats
+	if i64, err := runningCmd.Result(); err != nil {
+		return nil, err
+	} else {
+		stats.Running = i64
+	}
+
+	if i64, err := delayedCmd.Result(); err != nil {
+		return nil, err
+	} else {
+		stats.Delayed = i64
+	}
+
+	if i64, err := deadCmd.Result(); err != nil {
+		return nil, err
+	} else {
+		stats.Dead = i64
+	}
+
+	return &stats, nil
+}
